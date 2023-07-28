0f727295e9af8cf037c21e2a7b26f1d58fe9027f
Yotam Gur <yotamgur06@gmail.com>
2023-07-29 00:41:31 +0300

    Improved naming

--- FunctionalCS/CoInductiveLinkedList.cs
+++ FunctionalCS/CoInductiveLinkedList.cs
@@ -2,36 +2,36 @@
 {
     internal static class CoInductiveLinkedList
     {
-        public delegate void LinkedList<T>(Action nil, Action<T, LinkedList<T>> cons);
+        public delegate void LinkedList<T>(Action nilCallback, Action<T, LinkedList<T>> consCallback);
 
-        public static LinkedList<T> Nil<T>() => (nil, cons) => nil();
+        public static LinkedList<T> Nil<T>() => (nilCallback, consCallback) => nilCallback();
 
-        public static LinkedList<T> Cons<T>(T h, LinkedList<T> t) => (nil, cons) => cons(h, t);
+        public static LinkedList<T> Cons<T>(T h, LinkedList<T> t) => (nilCallback, consCallback) => consCallback(h, t);
 
         public static LinkedList<T> Filter<T>(this LinkedList<T> list, Func<T, bool> filter) =>
-            (nil, cons) => list(nil, (h, t) =>
+            (nilCallback, consCallback) => list(nilCallback, (h, t) =>
             {
-                if (filter(h)) cons(h, t.Filter(filter));
-                else t.Filter(filter)(nil, cons);
+                if (filter(h)) consCallback(h, t.Filter(filter));
+                else t.Filter(filter)(nilCallback, consCallback);
             });
 
         public static LinkedList<S> Map<T, S>(this LinkedList<T> list, Func<T, S> mapping) =>
-            (nil, cons) => list(nil, (h, t) => cons(mapping(h), t.Map(mapping)));
+            (nilCallback, consCallback) => list(nilCallback, (h, t) => consCallback(mapping(h), t.Map(mapping)));
 
         public static Action<Action<S>> Reduce<T, S>(this LinkedList<T> list, Func<S, T, S> reducer, S accumulator) =>
             action => list(() => action(accumulator), (h, t) => t.Reduce(reducer, reducer(accumulator, h))(action));
 
         public static LinkedList<T> Concat<T>(this LinkedList<T> list1, LinkedList<T> list2) =>
-            (nil, cons) => list1(() => list2(nil, cons), (h, t) => cons(h, t.Concat(list2)));
+            (nilCallback, consCallback) => list1(() => list2(nilCallback, consCallback), (h, t) => consCallback(h, t.Concat(list2)));
 
         public static void ForEach<T>(this LinkedList<T> list, Action<T> action) =>
             list(() => { }, (h, t) => { action(h); t.ForEach(action); });
 
         public static LinkedList<T> ToLinkedList<T>(this IEnumerable<T> enumerable) =>
-            (nil, cons) =>
+            (nilCallback, consCallback) =>
             {
-                if (enumerable.Any()) cons(enumerable.First(), enumerable.Skip(1).ToLinkedList());
-                else nil();
+                if (enumerable.Any()) consCallback(enumerable.First(), enumerable.Skip(1).ToLinkedList());
+                else nilCallback();
             };
     }
 }
